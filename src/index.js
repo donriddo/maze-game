96d6dc8b18f8b5cb8d42307682de1d5f08a93c75
donriddo <donriddo@gmail.com>
2018-04-01 11:24:18 +0100

    Add sanitization for dimension inputs

--- src/index.js
+++ src/index.js
@@ -284,14 +284,50 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  sanitizeInput(input, dimension) {
+    let output;
+    output = parseInt(input);
+    if (Number.isNaN(output)) {
+      return {
+        valid: false,
+        msg: `${input} not valid. 
+        Please enter a valid integer for ${dimension}`
+      };
+    } else if (output < 0) {
+      return {
+        valid: false,
+        msg: `${input} not valid. 
+          Please enter a positive integer for ${dimension}`
+      };
+    } else {
+      return {
+        valid: true,
+        output
+      };
+    }
+
+  }
   render() {
-    const width = parseInt(prompt("Please enter board width"));
-    const height = parseInt(prompt("Please enter board height"));
+    let width = this.sanitizeInput(
+      prompt("Please enter board width"),
+      'width'
+    );
+    while (!width.valid) {
+      width = this.sanitizeInput(prompt(width.msg), 'width');
+    }
+
+    let height = this.sanitizeInput(
+      prompt("Please enter board height"),
+      'height'
+    );
+    while (!height.valid) {
+      height = this.sanitizeInput(prompt(height.msg), 'height');
+    }
 
     return (
       <div className="game">
         <div className="game-board">
-          <Board width={width} height={height} />
+          <Board width={width.output} height={height.output} />
         </div>
       </div>
     );
